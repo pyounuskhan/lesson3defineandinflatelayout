3bce9d040080e5c527647297f817eb0add62ec90
pyounuskhan <pyounuskhan@gmail.com>
2018-03-02 10:59:12 +0530

    Initial commit

---
+++ app/src/main/java/ifirst/co/lesson3defineandinflatelayout/MainActivity.java
@@ -0,0 +1,20 @@
+package ifirst.co.lesson3defineandinflatelayout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+        public void onClickLeft(View view) {
+            setContentView(R.layout.activity_main2);
+    }
+    public void onClickRight(View view) {
+        setContentView(R.layout.activity_main);
+    }
+}
